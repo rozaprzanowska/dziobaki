78cdeec106aa6a3c96ab91e62d0dda522bfc2259
rozaprzanowska <42162019+rozaprzanowska@users.noreply.github.com>
2018-08-07 10:00:36 -0400

    Update dziob.R

--- dziob.R
+++ dziob.R
@@ -2,8 +2,9 @@ smpl <- rnorm(1000)
 
 xbar <- mean(x)
 s <- sd(x)
-
 hist(smpl)
 abline(v = xbar, lwd = 2, col = "red")
 abline(v = xbar + 2*s, lwd = 2, col = "red", lty = 3)
 abline(v = xbar - 2*s, lwd = 2, col = "red", lty = 3)
+
+#removed space
